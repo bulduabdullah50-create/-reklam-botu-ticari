eb09c2cbc693b99278a72eb41774d539113b92da
bulduabdullah50-create <bulduabdullah50@gmail.com>
2026-07-11 21:47:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import os
-import asyncio
-import re
 from telethon import TelegramClient, events
-
-# Railway Variables kısmından çekecek
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
+# Railway'den gelen bilgileri temiz (strip) bir şekilde al
+API_ID = int(os.getenv("API_ID").strip())
+API_HASH = os.getenv("API_HASH").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN").strip()
+# Botu başlat
 bot = TelegramClient('reklamci_ticari_session', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
-    await event.respond("Bot aktif! Yardım için /help yazın.")
-
+    await event.respond("Bot aktif!")
 print("Bot aktif ve sunucuda çalışıyor...")
 bot.run_until_disconnected()
